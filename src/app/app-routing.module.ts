8ae72d6be38a7bc938f02e5741f7b6abb2065456
Henro Potgieter <henro47@gmail.com>
2021-06-06 23:18:26 +0200

    Initial code for bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './Components/about/about.component';
+import { EducationComponent } from './Components/education/education.component';
+import { SkillsComponent } from './Components/skills/skills.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component: AboutComponent},
+  {path:'education',component:EducationComponent},
+  {path:'skills',component:SkillsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
