498d72b3101d3efe651f56bd6470059ac6bc6348
Henro Potgieter <henro47@gmail.com>
2021-06-07 17:39:28 +0200

    Cards Added

--- src/app/Components/about/about.component.ts
+++ src/app/Components/about/about.component.ts
@@ -8,8 +8,25 @@ import { Component, OnInit } from '@angular/core';
 export class AboutComponent implements OnInit {
 
   constructor() { }
+  //Main card info
   name = "Hendrik Fick Renaldo Potgieter (Henro)" ;
   education = "BSc Hons in Computer Science and Information Systems" ;
+  area = "NWU Potchefsroom Campus";
+  cell = "+27 79 923 1400";
+  email = "henro47@gmail.com";
+  download = "Download CV";
+
+  //About Card info
+  aboutTitle = "About";
+  aboutSubTitle = "A Litte about me";
+
+  //Career Card info
+  careerTitle = "Career";
+  careerSubTitle = "Goal Objective" ;
+
+  //Social Card info
+  socialTile = "Social";
+  socialSubTitle = "My Social Accounts";
 
   ngOnInit(): void {
   }
