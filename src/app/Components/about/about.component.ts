9e2549fa8420233a0e8a24eb8f99f1c0df5c5ad7
Henro Potgieter <henro47@gmail.com>
2021-06-09 15:41:11 +0200

    About page content

--- src/app/Components/about/about.component.ts
+++ src/app/Components/about/about.component.ts
@@ -21,16 +21,34 @@ export class AboutComponent implements OnInit {
 
   //About Card info
   aboutTitle = "About";
-  aboutSubTitle = "A Litte about me";
+  aboutSubTitle = "A Little about me";
+  aboutBody = "I am an independent, hardworking and creative individual " + 
+  "who is also reliable and has developed a responsible and ethical approach " +
+  "to the tasks that I undertake or any given situation I am placed in. " +
+  "I am excellent with working and communicating in a team. I believe in integrity " +
+  "and strong moral values and that an individual should strive to be successful not " +
+  "only in their career but also in their personal life. " +
+  "I also believe that everything you tackle you can be successful in " +
+  "if you do it with a positive manner and with hard work.";
 
   //Career Card info
   careerTitle = "Career";
   careerSubTitle = "Goal Objective" ;
+  careerBody = "I am looking for an opportunity to grow and learn in my field of study. " + 
+  "I understand that there is still much to learn, and I am willing and eager to learn new things. "+
+  "I like to solve complex problems and I try to solve them in a creative, unconventional way. " +
+  "When a problem grips me, I push myself to find a solution and I will not be satisfied until it is resolved. " +
+  "I am a hardworking, focused and disciplined individual with a big amount of will power. " +
+  "I have the correct amount of computer, communication and teamwork skills to be appointed this position." ;
 
   //Social Card info
   socialTile = "Social";
   socialSubTitle = "My Social Accounts";
+  gitHub = "GitHub";
+  linkedIn = "LinkedIn";
 
+  gitHubHref = "";
+  linkedInHref = "";
   ngOnInit(): void {
   }
 
