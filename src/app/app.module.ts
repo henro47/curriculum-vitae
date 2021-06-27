757fdadad89ec7558328e6bcca09f2f614b23eaf
Henro Potgieter <henro47@gmail.com>
2021-06-27 12:53:36 +0200

    References component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SkillsComponent } from './Components/skills/skills.component';
 import { EducationComponent } from './Components/education/education.component';
+import { ReferencesComponent } from './Components/references/references.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SkillsComponent,
-    EducationComponent
+    EducationComponent,
+    ReferencesComponent
   ],
   imports: [
     BrowserModule,
